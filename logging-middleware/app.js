79a4d3bac9ba7a61f327ca52036b07fe10545fcb
damodara2006 <damodara2006@gmail.com>
2026-07-08 13:00:39 +0530

    feat : added new request

--- logging-middleware/app.js
+++ logging-middleware/app.js
@@ -3,10 +3,83 @@ import dotenv from "dotenv";
 import { Log } from "./log.js";
 const app = express()
 dotenv.config();
-let AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJkYW1vZGFyYTIwMDZAZ21haWwuY29tIiwiZXhwIjoxNzgzNDk0ODI2LCJpYXQiOjE3ODM0OTM5MjYsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiI4YTMwYzQwZS1lN2Y1LTQ0MGItOTAyYi04NDU1NGIzMjU2ZDEiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJkYW1vZGFyYSBwcmFrYXNoIHAiLCJzdWIiOiIxZGE5MGMwMi01YTk4LTQ4ZTEtOWVmMy04YjYwMWI0YjBmYWEifSwiZW1haWwiOiJkYW1vZGFyYTIwMDZAZ21haWwuY29tIiwibmFtZSI6ImRhbW9kYXJhIHByYWthc2ggcCIsInJvbGxObyI6IjYxMTIyMzEwNDAyMyIsImFjY2Vzc0NvZGUiOiJ2VGF4dXIiLCJjbGllbnRJRCI6IjFkYTkwYzAyLTVhOTgtNDhlMS05ZWYzLThiNjAxYjRiMGZhYSIsImNsaWVudFNlY3JldCI6IkNqY2tNREhQWnlCbnNOd1IifQ.jd5tVcVipKvztyWV7GkUfuHc4N2qGBzi8DZwUl4UvGU"
-app.use(express.json());
-
+let AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJkYW1vZGFyYTIwMDZAZ21haWwuY29tIiwiZXhwIjoxNzgzNDk2NDcwLCJpYXQiOjE3ODM0OTU1NzAsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJlMzZjMWI5OC0wMTA1LTQ4YjItODFjMi0xZjRiNGI4ODJmOTAiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJkYW1vZGFyYSBwcmFrYXNoIHAiLCJzdWIiOiIxZGE5MGMwMi01YTk4LTQ4ZTEtOWVmMy04YjYwMWI0YjBmYWEifSwiZW1haWwiOiJkYW1vZGFyYTIwMDZAZ21haWwuY29tIiwibmFtZSI6ImRhbW9kYXJhIHByYWthc2ggcCIsInJvbGxObyI6IjYxMTIyMzEwNDAyMyIsImFjY2Vzc0NvZGUiOiJ2VGF4dXIiLCJjbGllbnRJRCI6IjFkYTkwYzAyLTVhOTgtNDhlMS05ZWYzLThiNjAxYjRiMGZhYSIsImNsaWVudFNlY3JldCI6IkNqY2tNREhQWnlCbnNOd1IifQ.G_hrikTymZCIwiCOxP4sGTxghXghaqn5ZjeMHkrdpwk"
 const url = "http://4.224.186.213/evaluation-service/notifications";
+
+app.get("/notifications/top", async (req, res) => {
+    await Log(
+        "backend",
+        "debug",
+        "handler",
+        "Fetching top 10 notifications"
+    );
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${AUTH_TOKEN}`
+            }
+        });
+
+        // console.log(await response.json());
+        
+        if (!response.ok) {
+            throw new Error(`API Error: ${response.status}`);
+        }
+
+        const notifications = await response.json();
+
+        // Priority Mapping
+        const priority = {
+            Placement: 3,
+            Result: 2,
+            Event: 1
+        };
+
+        console.log(notifications);
+        
+    
+        notifications.sort((a, b) => {
+
+            const priorityA = priority[a.type] || 0;
+            const priorityB = priority[b.type] || 0;
+
+            // Higher priority first
+            if (priorityA !== priorityB) {
+                return priorityB - priorityA;
+            }
+
+            // Same priority -> Latest notification first
+            return new Date(b.createdAt) - new Date(a.createdAt);
+        });
+
+        // Get Top 10
+        const topNotifications = notifications.slice(0, 10);
+
+        await Log(
+            "backend",
+            "info",
+            "handler",
+            "Top 10 notifications fetched successfully"
+        );
+
+        res.status(200).json(topNotifications);
+
+    } catch (error) {
+
+        await Log(
+            "backend",
+            "error",
+            "handler",
+            error.message
+        );
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
 
 app.get("/notifications", async (req, res) => {
     await Log(
